ca1374292053db3f91e1286fbee995f349bbad3e
mylzagaen <madridmiigaa@gmail.com>
2022-03-22 11:42:27 +0800

    lia

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <div className={styles.body}>
+
+    <div className={styles.container}>
+        
+        <div className={styles.user_info}>
+          <img src="https://img.icons8.com/dusk/64/000000/circled-user-female-skin-type-7.png"  className={styles.user}/>
+          <img src='https://img.icons8.com/color/344/vertical-line.png' className={styles.line}/>
+          <img src='https://img.icons8.com/clouds/344/information.png' className={styles.user}/>
+          <img src='https://img.icons8.com/color/344/vertical-line.png' className={styles.line}/>
+          <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-education-job-search-flaticons-lineal-color-flat-icons-3.png' className={styles.user}/>
+          <img src='https://img.icons8.com/color/344/vertical-line.png' className={styles.line}/>
+          <img src='https://img.icons8.com/external-flaticons-lineal-color-flat-icons/2x/external-hobby-lifestyles-flaticons-lineal-color-flat-icons-3.png' className={styles.user}/>
+          <img src='https://img.icons8.com/color/344/vertical-line.png' className={styles.line}/>
+        </div>
+
+      <div className={styles.info}>
+        <h1 className={styles.title}> Э.Мягмарсүрэн </h1>
+        <img src='https://img.icons8.com/fluency/344/wavy-line.png' className={styles.wave}/>
+        <p className={styles.title}><img src='https://img.icons8.com/external-bartama-outline-32-bartama-graphic/344/external-card-school-bartama-outline-32-bartama-graphic.png' className={styles.icon}/> Оюутан</p>
+        <p className={styles.title}><img src='https://img.icons8.com/external-smashingstocks-glyph-smashing-stocks/344/external-computer-stationery-smashingstocks-glyph-smashing-stocks.png' className={styles.icon}/> Ирээдүйн инженер</p>
+        <h1 className={styles.title}> Information </h1>
+        <img src='https://img.icons8.com/fluency/344/wavy-line.png' className={styles.wave}/>
+        <p className={styles.title}>Би 20 настай. Жинлүүрийн орд. Дээрээ ганц ахтай, айлын бага охин.
+        {/* <h1 className={styles.title}>Contact Info</h1> */}
+        <p className={styles.title}><img src='https://img.icons8.com/material-outlined/344/facebook-new.png' className={styles.icon}/> Enkhbold Myagmarsuren  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; <img src='https://img.icons8.com/ios/344/apple-phone.png' className={styles.icon}/> 85958814</p> 
+        <p className={styles.title}><img src='https://img.icons8.com/material-outlined/344/gmail-new.png' className={styles.icon} /> madridmiigaa@gmail.com &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='https://img.icons8.com/material-outlined/344/instagram-new--v2.png' className={styles.icon}/> mnegun </p>
+        </p>
+        <h1 className={styles.title}> Education </h1>
+        <img src='https://img.icons8.com/fluency/344/wavy-line.png' className={styles.wave}/>
+        <p className={styles.title}>1-11-р анги хүртлээ нийт 4 улсын, 2 хувийн сургууль хооронд шилжсэн. <br/> 12-р ангиа БГД-ийн Норовлин сургуульд 2019 онд төгссөн. <br/> Одоогоор Монгол Улсын Их Сургуульд Програм хангамжийн чиглэлгээр 3-р курсд суралцаж байна. <br/> &nbsp;</p>
+        <h1 className={styles.title}> Hobby </h1>
+        <img src='https://img.icons8.com/fluency/344/wavy-line.png' className={styles.wave}/>
+        <p className={styles.title}>Чөлөөт цагаараа ном унших, шоо эвлүүлэх, сонгодог хөгжим сонсох дуртай. Мөн  миний хамгийн дуртай зүйл эрхлэх бас эрхлүүлэх юмуу даа. Жоохон хүүхэд, аав хоёрынхоо хажууд хэлгүй дада болчихдог. Бас найзуудтайгаа кареокедах дуртай.</p>
+      </div>
+
+      <div className={styles.last}>
+      <div className={styles.cv}>
+          <h1 className={styles.Head}>My <br/> CV</h1>
+        </div>
+        <div>
+        <h1 className={styles.lan}>Language</h1>
+        <p className={styles.lantit}>English &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/></p>
+        <p className={styles.lantit}>Korean&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/></p>
+        <p className={styles.lantit}>Portuguese &nbsp; <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/></p>
+        <p className={styles.lantit}>Japanese &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/> <img className={styles.icon1} src='https://img.icons8.com/plasticine/344/like--v2.png'/></p>
+        </div>
+
+        
+       
+        {/* <div>
+        <h1 className={styles.lan}>Skills</h1>
+        <p className={styles.lantit}>Teamwork &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/></p>
+        <p className={styles.lantit}>Creativity &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/color/344/star--v1.png'/></p>
+        <p className={styles.lantit}>Problem Solving&nbsp;&nbsp;&nbsp;&nbsp; <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/color/344/star--v1.png'/> <img className={styles.icon1} src='https://img.icons8.com/color/344/star--v1.png'/></p>
+        <p className={styles.lantit}>Time Management&nbsp;<img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/fluency/344/star.png'/> <img className={styles.icon1} src='https://img.icons8.com/color/344/star--v1.png'/></p>
+        </div> */}
+      </div> 
+       
+      
+      </div>
+      </div>
+    
+  )
+}
